0aba076086881c08ea44860b08bf0cf7caabe42a
davydov_a <davydov_a@eragadesign.com>
2015-05-26 14:00:34 +0300

    refs #1514 @10 Добавили сущность "Звонок"

---
+++ classes/call/schema.php
@@ -0,0 +1,31 @@
+<?php
+
+$schema['taxi_office_calls'] = array(
+		'description' => 'TaxiOffice incoming calls base table',
+		'fields'      => array(
+				'id'        => array(
+						'type'     => 'serial',
+						'not null' => TRUE,
+						'unsigned' => TRUE,
+				),
+				'hang_time' => array(
+						'type'     => 'int',
+						'not null' => TRUE,
+						'unsigned' => TRUE,
+				),
+				'operator'  => array(
+						'type'     => 'int',
+						'not null' => TRUE,
+						'unsigned' => TRUE,
+						'default'  => 0,
+				),
+
+				'status'    => array(
+						'type'     => 'int',
+						'not null' => TRUE,
+						'unsigned' => TRUE,
+						'default'  => 0,
+				),
+		),
+		'primary key' => array('id'),
+);
