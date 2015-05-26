449ad250bbbf2bb7f5dab137a6e0b1d7241a6f51
davydov_a <davydov_a@eragadesign.com>
2015-05-26 12:37:07 +0300

    Добавили фугкцию подключения файлов

---
+++ _functions.php
@@ -0,0 +1,10 @@
+<?php
+
+
+function _taxi_office_include_settings($file_name = NULL) {
+	$path_to_module = drupal_get_path('module', 'taxi_office') . '/classes';
+	$subdirs = array_filter(glob($path_to_module . '/*', GLOB_ONLYDIR), 'is_dir');
+	foreach ($subdirs as $dirname) {
+		@include_once $dirname . '/' . $file_name . '.php';
+	}
+}
