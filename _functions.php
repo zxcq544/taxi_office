a3a9fb1fcc31e64523e1e0bf999a8205a6798ee6
davydov_a <davydov_a@eragadesign.com>
2015-05-27 14:22:31 +0300

    инклудим drupal way

--- _functions.php
+++ _functions.php
@@ -1,11 +1,11 @@
 <?php
 
 
-function _taxi_office_include_settings($file_name = NULL, &$schema) {
+function _taxi_office_includes($file_name = NULL, &$schema) {
 	$path_to_module = drupal_get_path('module', 'taxi_office') . '/classes';
 	$subdirs = array_filter(glob($path_to_module . '/*', GLOB_ONLYDIR), 'is_dir');
 	foreach ($subdirs as $dirname) {
-		@include_once $dirname . '/' . $file_name . '.php';
+		module_load_include('php', 'taxi_office', $file_name);
 		$function_name = '__' . basename($dirname) . '_' . $file_name;
 		if (function_exists($function_name))
 			$function_name($schema);
