d4a0b9857980490dc5710a5870368b75ccaadb10
davydov_a <davydov_a@eragadesign.com>
2015-05-27 13:08:42 +0300

    Добавили стандартные настройки для переопределения buildContent

--- _functions.php
+++ _functions.php
@@ -11,3 +11,22 @@ function _taxi_office_include_settings($file_name = NULL, &$schema) {
 			$function_name($schema);
 	}
 }
+
+function _taxi_office_view_field_defaults(
+		Entity $entity,
+		$label_display = 'above',
+		$field_type = 'text',
+		$formatter = 'text_default') {
+	return array(
+			'#theme'         => 'field',
+			'#weight'        => 0,
+			'#access'        => TRUE,
+			'#label_display' => $label_display,
+			'#view_mode'     => 'full',
+			'#language'      => LANGUAGE_NONE,
+			'#field_type'    => $field_type,
+			'#entity_type'   => $entity->entityType(),
+			'#bundle'        => $entity->bundle(),
+			'#formatter'     => $formatter,
+	);
+}
