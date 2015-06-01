c7a6a6f13e4f5d8ca51e2fa07d3d89b014a2311e
sergey <zxcq544@yandex.ru>
2015-06-01 17:42:23 +0300

    Кнопка заказов вроде работает. Осталось выбрать правильные поля, сделать валидацию полей формы и т.п.

--- template/order_button_to_get_form_by_ajax.tpl.php
+++ template/order_button_to_get_form_by_ajax.tpl.php
@@ -1,14 +1,41 @@
 <!-- Button trigger modal -->
 <button id="get_form_content" type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-	Launch demo modal
+	Заказать
 </button>
 <script>
+	var order_form;
+	var send_button;
 	var get_form_content_button = document.getElementById('get_form_content');
+
+	//Получение формы после нажатия на кнопку Заказать
 	get_form_content_button.addEventListener('click', function () {
 		jQuery.get("/ajax_order_page", function (data) {
 			jQuery("#modal-body-id").html(data);
+			send_button = document.getElementById('edit-submit');
+			order_form = document.getElementById('order-form');
+			order_form.addEventListener('submit', function (ev) {
+				ev.preventDefault();
+				send_form_by_ajax();
+			});
 		});
 	});
+
+
+	//функция для отправки формы заказа по аякс
+	function send_form_by_ajax() {
+		var xhr = new XMLHttpRequest();
+		xhr.onload = function (ev) {
+			if (xhr.status == 200) {
+				window.location = "/dispatcher";
+			} else {
+				alert("Error " + xhr.status + " occurred uploading your file.<br \/>");
+			}
+		};
+		xhr.open('post', '/ajax_order_page');
+		xhr.send(new FormData(order_form));
+	}
+
+
 </script>
 <!-- Modal -->
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
